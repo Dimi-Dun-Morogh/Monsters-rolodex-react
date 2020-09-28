d0bd7d739da4c6ba1d1025ae858ed9d0a8a19957
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-09-28 19:29:45 +0300

    Updates

---
+++ precache-manifest.f17c03b4d96cb744417289d6001782f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393a698d870496c51f1b21961319b9ec",
+    "url": "/Monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "4e14e88961ee57d4a86a",
+    "url": "/Monsters-rolodex-react/static/css/main.8fa62c24.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-rolodex-react/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex-react/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e14e88961ee57d4a86a",
+    "url": "/Monsters-rolodex-react/static/js/main.b785559e.chunk.js"
+  },
+  {
+    "revision": "18e25c8f1c9d17614729",
+    "url": "/Monsters-rolodex-react/static/js/runtime-main.a0b5e574.js"
+  }
+]);
